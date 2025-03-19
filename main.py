4a6b2c102aa2b4c74652a4cc30dd7c1b09be4bdb
sergiolttz <sergiolattanzio05@gmail.com>
2025-03-19 17:32:17 -0400

    primer commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"Hello": "World"}
+
+class AgenteBasico(BaseModel):
+    prompt: str
+        
+
+@app.post("/agente-basico")
+async def agente_basico(prompt: AgenteBasico):
+    return {"prompt": prompt}
